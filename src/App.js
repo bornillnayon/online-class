662b7ff90bf7e9dd49507deb622276c5f8b7de9f
bornillnayon <nayonbd15@gmail.com.com>
2020-09-23 00:08:02 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header';
+import Bodyone from './Components/Body1/Bodyone';
+import Course from './Components/Course/Course';
+
+
+function App()  {
+ 
+    return (
+      <div className="container">
+        <Header></Header>
+        <Bodyone></Bodyone>
+        <Course></Course>
+      </div>
+    );
+  } 
+export default App;
